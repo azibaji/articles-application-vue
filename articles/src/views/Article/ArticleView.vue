eabbe22b7a26951955c6551bf24d6335e00d3154
Arezou Saremian <arezoosaremian71@gmail.com>
2022-04-20 11:58:52 +0430

    Fix article view page like clear comment box and change some functions.

--- articles/src/views/Article/ArticleView.vue
+++ articles/src/views/Article/ArticleView.vue
@@ -37,7 +37,7 @@
                             <path d="M256 32C114.6 32 .0272 125.1 .0272 240c0 47.63 19.91 91.25 52.91 126.2c-14.88 39.5-45.87 72.88-46.37 73.25c-6.625 7-8.375 17.25-4.625 26C5.818 474.2 14.38 480 24 480c61.5 0 109.1-25.75 139.1-46.25C191.1 442.8 223.3 448 256 448c141.4 0 255.1-93.13 255.1-208S397.4 32 256 32zM256.1 400c-26.75 0-53.12-4.125-78.38-12.12l-22.75-7.125l-19.5 13.75c-14.25 10.12-33.88 21.38-57.5 29c7.375-12.12 14.37-25.75 19.88-40.25l10.62-28l-20.62-21.87C69.82 314.1 48.07 282.2 48.07 240c0-88.25 93.25-160 208-160s208 71.75 208 160S370.8 400 256.1 400z"/>
                         </svg>
                     </div>
-                    <div class="vote " @click="submitVote">
+                    <div class="vote " @click="AddToFavorite">
                         {{article.favoritesCount}}
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" v-if="!votedFlag">
                             <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z"/>
@@ -83,12 +83,14 @@
                     <label for="exampleFormControlTextarea1">Write your comment</label>
                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" v-model="commentText"></textarea>
                 </div>
+
                 <button v-if="!newCommentFlag" @click="addNewComment">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                         <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/>
                     </svg>
                     New commnet
                 </button>
+
                 <button v-else @click="postComment">
                     send
                 </button>
@@ -129,7 +131,8 @@ export default {
 
         getComments(){
             this.axios.get(`${process.env.VUE_APP_URL}/articles/${this.slug}/comments`)
-             .then( response => this.comments= response.data.comments)
+             .then( response => this.comments = response.data.comments)
+             .catch(error => console.log(error) )
         },
 
         formatDate(date){
@@ -152,12 +155,14 @@ export default {
             this.axios.post(`${process.env.VUE_APP_URL}/articles/${this.slug}/comments`, comment, {headers} )
                 .then( response => {
                      response.data.comment && this.$toast.success('Your comment was sent successfully. :)')
+                     this.commentText = ''
                 })
                 .catch( error => {
                         error.response && this.$toast.error(error.response.statusText)
                 })
          },
-         submitVote(){
+
+         AddToFavorite(){
              if(this.user.email){
                 this.votedFlag = !this.votedFlag
                 const headers = {
