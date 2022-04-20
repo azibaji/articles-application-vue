02ad7f116876bc3a96dfda1fd6f3831eb12753e4
Arezou Saremian <arezoosaremian71@gmail.com>
2022-04-20 11:59:48 +0430

    Chane variable name in tags view.

--- articles/src/views/Article/TagView.vue
+++ articles/src/views/Article/TagView.vue
@@ -1,20 +1,25 @@
 <template>
     <div class="tag">
         <div class="tag__header">
+
             <div class="back">
-                <router-link :to="{ name: 'article', params: { slug: pastArticle }}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path d="M9.375 233.4l128-128c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H480c17.69 0 32 14.31 32 32s-14.31 32-32 32H109.3l73.38 73.38c12.5 12.5 12.5 32.75 0 45.25c-12.49 12.49-32.74 12.51-45.25 0l-128-128C-3.125 266.1-3.125 245.9 9.375 233.4z"/>
-                </svg>
-                Back
-            </router-link>
+                <router-link :to="{ name: 'article', params: { slug: slug }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path d="M9.375 233.4l128-128c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H480c17.69 0 32 14.31 32 32s-14.31 32-32 32H109.3l73.38 73.38c12.5 12.5 12.5 32.75 0 45.25c-12.49 12.49-32.74 12.51-45.25 0l-128-128C-3.125 266.1-3.125 245.9 9.375 233.4z"/>
+                    </svg>
+                    Back
+                </router-link>
             </div>
+
             <div class="info">
                 <p>Related articles to :</p>
                 <h2>{{tag}}</h2>
             </div>
+
         </div>
+
         <div class="tag__body">
+
              <div class="articles__wrapper">
                 <article-item :article="article" v-for="(article, index) in articles" :key="index"/>
             </div>
@@ -30,7 +35,7 @@ export default {
     data(){
         return{
             tag: this.$route.params.tagName,
-            pastArticle: this.$route.params.articleName,
+            slug: this.$route.params.articleName,
             articles:[]
         }
     },
